50faaef47618e1383d6db71d6366c1db751fe439
FilipVidovic763 <63953605+FilipVidovic763@users.noreply.github.com>
2020-06-21 23:11:26 +0200

    ESP_projekt

---
+++ ESP/studenti/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+class studenti(models.Model):
+    naziv_tvrtke= models.CharField(max_length=100)
+    struka=models.CharField(max_length=100)
+    naziv_tvrtke_za_praksu=models.CharField(max_length=100)
+    opis_zadataka=models.TextField()
+    datum_objave=models.DateTimeField(default=timezone.now)
+    author=models.ForeignKey(User, on_delete=models.CASCADE)
+    
